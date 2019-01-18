dfa96600f703a4521c6fcbb97e5a1b2933cce983
HanYong <HanYong@192.168.1.102>
2019-01-18 22:08:11 +0800

    POD

---
+++ Example/Pods/Target Support Files/HYPodTestDemo/HYPodTestDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HYPodTestDemo : NSObject
+@end
+@implementation PodsDummy_HYPodTestDemo
+@end
---
+++ Example/Pods/Target Support Files/Pods-HYPodTestDemo_Example/Pods-HYPodTestDemo_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HYPodTestDemo_Example : NSObject
+@end
+@implementation PodsDummy_Pods_HYPodTestDemo_Example
+@end
